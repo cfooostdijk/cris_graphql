0da9eb28e5bc27bae5ebd3a10d6b73f2aea2388b
Cristian <co@qubikdigital.com>
2021-10-19 16:51:20 -0300

    Graph objects added

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,17 +1,9 @@
 module Types
   class QueryType < Types::BaseObject
-    # Add `node(id: ID!) and `nodes(ids: [ID!]!)`
-    include GraphQL::Types::Relay::HasNodeField
-    include GraphQL::Types::Relay::HasNodesField
+      field :all_orders, [Types::OrderType], null: false
 
-    # Add root-level fields here.
-    # They will be entry points for queries on your schema.
-
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
-    end
+      def all_orders
+          Order.all
+      end
   end
 end
